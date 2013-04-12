9253adf23b751171ad8e9cbe2b2b9b9dd3b7066c
David Ed Mellum <david@edmellum.com>
2013-04-12 14:29:22 +0200

    Fix chat tests

--- chat/test.js
+++ chat/test.js
@@ -19,7 +19,7 @@ describe('Chat App', function() {
   });
 
   it('should announce new users', function(done) {
-    client1.on('new user', function(data) {
+    client1.once('new user', function(data) {
       assert.equal(data, 'New user joined!');
       done();
     });
@@ -27,36 +27,38 @@ describe('Chat App', function() {
   });
 
   it('should broadcast messages to other users', function(done) {
-    client1.on('message', function(data) {
+    client1.once('message', function(data) {
       assert.deepEqual(data, {text: 'Do you see me?'});
       done();
     });
 
     client2 = io.connect(appUrl, ioOptions);
-    client2.on('connect', function() {
+    client2.once('connect', function() {
       client2.emit('message', 'Do you see me?');
     });
   });
 
   it('should let users register a username', function(done) {
-    client1.on('message', function(data) {
+    client1.once('message', function(data) {
       assert.deepEqual(data, {username: 'brodude232', text: 'yo bro'});
+      done();
     });
 
     client2 = io.connect(appUrl, ioOptions);
-    client2.on('connect', function() {
+    client2.once('connect', function() {
       client2.emit('username', 'brodude232');
       client2.emit('message', 'yo bro');
     });
   });
 
   it('should let users create and join rooms', function(done) {
-    client1.on('message', function(data) {
+    client1.once('message', function(data) {
       assert.deepEqual(data, {text: 'yo bro'});
+      done();
     });
 
     client2 = io.connect(appUrl, ioOptions);
-    client2.on('connect', function() {
+    client2.once('connect', function() {
       // Implicit creation when joining a non-existent room.
       client2.emit('room', 'coolpeople');
       client1.emit('room', 'coolpeople');
